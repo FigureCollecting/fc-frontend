038557a9c003f793cbc915e820b3288b0b5d1328
Ross Goldberg <rpgoldberg@gmail.com>
2026-09-01 18:30:22 -0500

    Migrate jest 27 to 30 (jsdom 26); drop CRA-era exports resolver and camelcase; pin nwsapi 2.2.24

--- jest/fileTransform.cjs
+++ jest/fileTransform.cjs
@@ -1,35 +1,30 @@
 'use strict';
 
 // Jest transformer turning asset imports into filenames (and SVGs into a
-// React component). Mirrors Create React App's config/jest/fileTransform.js.
+// React component). Jest-30-native replacement for CRA's fileTransform.
 
 const path = require('path');
-const camelcase = require('camelcase');
+
+const pascalCase = (name) =>
+  name
+    .split(/[^a-zA-Z0-9]+/)
+    .filter(Boolean)
+    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+    .join('');
 
 module.exports = {
   process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
 
     if (filename.match(/\.svg$/)) {
-      const pascalCaseFilename = camelcase(path.parse(filename).name, {
-        pascalCase: true,
-      });
-      const componentName = `Svg${pascalCaseFilename}`;
+      const componentName = `Svg${pascalCase(path.parse(filename).name)}`;
       return {
         code: `const React = require('react');
       module.exports = {
         __esModule: true,
         default: ${assetFilename},
         ReactComponent: React.forwardRef(function ${componentName}(props, ref) {
-          return {
-            $$typeof: Symbol.for('react.element'),
-            type: 'svg',
-            ref: ref,
-            key: null,
-            props: Object.assign({}, props, {
-              children: ${assetFilename}
-            })
-          };
+          return React.createElement('svg', Object.assign({ ref: ref }, props), ${assetFilename});
         }),
       };`,
       };
